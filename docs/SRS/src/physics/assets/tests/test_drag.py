e8a5105333468b4a4ec36172a4e5c1e27b5fecab
Ahmed-Mohamed-soliman <ma9417252@gmail.com>
2026-07-10 16:03:55 +0300

    Add test for calculate_drag function

--- docs/SRS/src/physics/assets/tests/test_drag.py
+++ docs/SRS/src/physics/assets/tests/test_drag.py
@@ -1,1 +1,14 @@
+from src.physics.drag import calculate_drag
 
+
+def test_calculate_drag():
+    drag = calculate_drag(
+        rho=1.225,
+        velocity=70,
+        wing_area=16,
+        cd=0.03
+    )
+
+    expected = 1440.6
+
+    assert abs(drag - expected) < 0.01
